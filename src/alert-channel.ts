f3c47f6025816dd1e7b471195686a5726da990b4
Team Terraform CDK <github-team-tf-cdk@hashicorp.com>
2022-08-09 03:34:26 +0000

    chore: upgrade provider

--- src/alert-channel.ts
+++ src/alert-channel.ts
@@ -27,7 +27,7 @@ export interface AlertChannelConfig extends cdktf.TerraformMetaArguments {
   */
   readonly name: string;
   /**
-  * (Required) The type of channel. One of: (pagerduty, slack, user, victorops, webhook, email, opsgenie).
+  * (Required) The type of channel. One of: (webhook, email, opsgenie, pagerduty, slack, user, victorops).
   * 
   * Docs at Terraform Registry: {@link https://www.terraform.io/docs/providers/newrelic/r/alert_channel#type AlertChannel#type}
   */
