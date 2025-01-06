02d349772fe8609337ed4836f0c539b2ba684630
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-01-06 17:26:17 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@a6d62ea

--- src/cloud-gcp-link-account/index.ts
+++ src/cloud-gcp-link-account/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/newrelic/newrelic/3.53.0/docs/resources/cloud_gcp_link_account
 // generated from terraform resource schema
 
