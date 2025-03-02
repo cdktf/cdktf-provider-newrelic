4b5aadab829035dafd79a069e3f582895aac5766
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-03-02 06:08:46 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@b33609d

--- src/browser-application/index.ts
+++ src/browser-application/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/newrelic/newrelic/3.57.0/docs/resources/browser_application
 // generated from terraform resource schema
 
