26e9eaf822ecfbc0790ec0f9471e4d691e0d3083
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-03-06 03:19:56 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/events-to-metrics-rule/index.ts
+++ src/events-to-metrics-rule/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/newrelic/newrelic/3.57.1/docs/resources/events_to_metrics_rule
 // generated from terraform resource schema
 
