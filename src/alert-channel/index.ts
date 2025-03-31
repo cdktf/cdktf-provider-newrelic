0ee5aac7aa870b6333f74fb78c71446df4c4721f
team-tf-cdk <84392119+team-tf-cdk@users.noreply.github.com>
2025-03-31 15:33:08 +0200

    chore(deps): update  by cdktf/cdktf-repository-manager@edbf1a8 (#2372)
    Triggered by
    https://github.com/cdktf/cdktf-repository-manager/actions/runs/14172000331
    
    ---------
    
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/alert-channel/index.ts
+++ src/alert-channel/index.ts
@@ -32,7 +32,7 @@ export interface AlertChannelConfig extends cdktf.TerraformMetaArguments {
   */
   readonly name: string;
   /**
-  * (Required) The type of channel. One of: (webhook, email, opsgenie, pagerduty, slack, user, victorops).
+  * (Required) The type of channel. One of: (user, victorops, webhook, email, opsgenie, pagerduty, slack).
   *
   * Docs at Terraform Registry: {@link https://registry.terraform.io/providers/newrelic/newrelic/3.60.0/docs/resources/alert_channel#type AlertChannel#type}
   */
